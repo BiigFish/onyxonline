c63d351019b75cc4ac641fd11356c2b18c9c910d
BiigFish <83352674+BiigFish@users.noreply.github.com>
2021-09-01 16:22:10 -0400

    done finally pls

--- assets/js/web-design-buttons.js
+++ assets/js/web-design-buttons.js
@@ -4,12 +4,12 @@ function changeTextBlog() {
     x.innerHTML = "Write and post blogs for all to see";
     document.getElementById("blog").style.margin = "-10px";
     document.getElementById("blog-circle").style.padding = "100px 62px";
-    document.getElementById("blog-circle").style.margin = "-0 50px";
+    document.getElementById("blog-circle").style.margin = "-0 auto";
   } else {
     x.innerHTML = "Blog";
     document.getElementById("blog").style.margin = "0";
     document.getElementById("blog-circle").style.padding = "50px 12px";
-    document.getElementById("blog-circle").style.margin = "50px 100px";
+    document.getElementById("blog-circle").style.margin = "50px auto";
   }
 }
 
@@ -19,12 +19,12 @@ function changeTextStore() {
       x.innerHTML = "Display products and services for customers to pay and purchase";
       document.getElementById("store").style.margin = "-30px";
       document.getElementById("store-circle").style.padding = "100px 62px";
-      document.getElementById("store-circle").style.margin = "-0 50px";
+      document.getElementById("store-circle").style.margin = "0 auto";
     } else {
       x.innerHTML = "Store Front";
       document.getElementById("store").style.margin = "0";
       document.getElementById("store-circle").style.padding = "50px 12px";
-      document.getElementById("store-circle").style.margin = "50px 100px";
+      document.getElementById("store-circle").style.margin = "50px auto";
     }
   }
 function changeTextGallery() {
@@ -33,12 +33,12 @@ function changeTextGallery() {
       x.innerHTML = "Showcase photography or artwork with no distractions";
       document.getElementById("gallery").style.margin = "-30px";
       document.getElementById("gallery-circle").style.padding = "100px 62px";
-      document.getElementById("gallery-circle").style.margin = "-0 50px";
+      document.getElementById("gallery-circle").style.margin = "-0 auto";
     } else {
       x.innerHTML = "Gallery";
       document.getElementById("gallery").style.margin = "0";
       document.getElementById("gallery-circle").style.padding = "50px 12px";
-      document.getElementById("gallery-circle").style.margin = "50px 100px";
+      document.getElementById("gallery-circle").style.margin = "50px auto";
     }
   }
 function changeTextPortfolio() {
@@ -47,11 +47,11 @@ function changeTextPortfolio() {
     x.innerHTML = "Leave an impression to the world with your own site";
     document.getElementById("portfolio").style.margin = "-25px";
     document.getElementById("portfolio-circle").style.padding = "100px 62px";
-    document.getElementById("portfolio-circle").style.margin = "-0 50px";
+    document.getElementById("portfolio-circle").style.margin = "-0 auto";
   } else {
     x.innerHTML = "Portfolio";
     document.getElementById("portfolio").style.margin = "0";
     document.getElementById("portfolio-circle").style.padding = "50px 12px";
-    document.getElementById("portfolio-circle").style.margin = "50px 100px";
+    document.getElementById("portfolio-circle").style.margin = "50px auto";
   }
 }
